8b8705c8f7adaca1f89b5a58a1a5c682bf0c2743
vmomparler1 <87720857+vmomparler1@users.noreply.github.com>
2025-07-13 20:36:47 +0200

    fixing webhook

--- app/api/stripe-webhook/route.ts
+++ app/api/stripe-webhook/route.ts
@@ -18,7 +18,7 @@ export async function POST(request: NextRequest) {
     }
 
     const body = await request.text();
-    const headersList = headers();
+    const headersList = await headers();
     const signature = headersList.get("stripe-signature");
 
     if (!signature) {
